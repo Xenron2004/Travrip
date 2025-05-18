5b25b408b6f518bbe2fff8896ef6d9f7047945a6
Xenron2004 <kumarkoushal787075@gmail.com>
2025-05-18 06:44:17 +0530

    full

--- src/components/Bg.jsx
+++ src/components/Bg.jsx
@@ -6,14 +6,14 @@ import Home from "./Home";
 
 const Bg = () => {
   return (
-    <div className="flex max-w-[1280px] mt-4 border-t border-dotted border-gray-300 pt-2 w-11/12 items-center justify-between text-[#212B36] text-[14px] font-sans font-regular ">
+    <div className="flex max-w-[1280px] mt-4 border-t border-dotted border-gray-300 pt-2 w-11/12 items-center justify-between text-white text-[14px] font-sans font-regular ">
       <div>
         Designed and developed by{" "}
         <a
           href="https://www.reevij.com/"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-primary underline"
+          className="text-white underline"
         >
           Reevij Solutions
         </a>
