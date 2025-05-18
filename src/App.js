e78c6e5a6a9d97e96f85b95b5eb8198af378706f
Xenron2004 <kumarkoushal787075@gmail.com>
2025-05-18 06:36:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Home from "./components/Home";
 import { useEffect } from "react";
 
+// import LocomotiveScroll from "locomotive-scroll";
+// import "locomotive-scroll/dist/locomotive-scroll.css";
+
 function App() {
-    
- 
+  // useEffect(() => {
+  //   const scroll = new LocomotiveScroll({
+  //     el: document.querySelector("[data-scroll-container]"),
+  //     smooth: true,
+  //   });
+
+  //   return () => {
+  //     scroll.destroy();
+  //   };
+  // }, []);
   return (
-    <div  className=" ">
-      <Home/>
+    <div className=" ">
+      <Home />
     </div>
   );
 }
---
+++ src/components/Bg.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Home from "./Home";
+
+
+
+const Bg = () => {
+  return (
+    <div className="flex max-w-[1280px] mt-4 border-t border-dotted border-gray-300 pt-2 w-11/12 items-center justify-between text-[#212B36] text-[14px] font-sans font-regular ">
+      <div>
+        Designed and developed by{" "}
+        <a
+          href="https://www.reevij.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-primary underline"
+        >
+          Reevij Solutions
+        </a>
+      </div>
+      © 2024 Payswift Corp. All rights reserved
+    </div>
+  );
+};
+
+export default Bg;
